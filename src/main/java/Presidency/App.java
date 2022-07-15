9b8d6edd3f6cf08968548f43c5570eb36ada25e6
D37467K <48399511+D37467K@users.noreply.github.com>
2022-07-15 14:43:39 +0530

    Update App.java

--- src/main/java/Presidency/App.java
+++ src/main/java/Presidency/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Presidency!" );
+        System.out.println( "Hello Presidency1!" );
     }
 }
